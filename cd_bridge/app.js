735a8de8d1633825d31c5dbb06f897791f44db2e
Halifax-FX <choice1_3part_vodka@hotmail.com>
2022-12-20 15:36:35 +1300

    Initialise webapp

---
+++ cd_bridge/app.js
@@ -0,0 +1,30 @@
+// Setup Express
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// Setup Handlebars
+const handlebars = require("express-handlebars");
+app.engine("handlebars", handlebars({
+    defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+// Setup body-parser
+app.use(express.urlencoded({ extended: false }));
+
+// Setup cookie-parser
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
+
+// Make the "public" folder available statically
+const path = require("path");
+app.use(express.static(path.join(__dirname, "public")));
+
+// Setup routes
+app.use(require("./routes/routes.js"));
+
+// Start the server running.
+app.listen(port, function () {
+    console.log(`App listening on port ${port}!`);
+});
